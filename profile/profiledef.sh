352d78768b61bd9524fb17f7f559992148249c38
carjam120443-netizen <carjam120443@gmail.com>
2026-09-18 17:44:44 -0400

    build: add CarsonArch base profile/profiledef.sh

---
+++ profile/profiledef.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+iso_name="carsonarch"
+iso_label="CARSONARCH"
+iso_publisher="CarsonArch Project"
+iso_application="CarsonArch Live/Install Media"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+
+buildmodes=('iso')
+bootmodes=('bios.syslinux' 'uefi-x64.systemd-boot')
+archisobasedir="$install_dir"
