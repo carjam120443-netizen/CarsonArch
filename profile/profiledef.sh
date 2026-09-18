9dcb6838f000606abc0077af54b58266caf6f93f
carjam120443-netizen <carjam120443@gmail.com>
2026-09-18 17:47:56 -0400

    fix: define required ArchISO profile paths

--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2034
 
 iso_name="carsonarch"
 iso_label="CARSONARCH"
 iso_publisher="CarsonArch Project"
 iso_application="CarsonArch Live/Install Media"
-iso_version="$(date +%Y.%m.%d)"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 
-buildmodes=('iso')
-bootmodes=('bios.syslinux' 'uefi.systemd-boot')
-archisobasedir="$install_dir"
+bootmodes=('bios.syslinux'
+           'uefi.systemd-boot')
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86,arm64' '-b' '1M' '-Xdict-size' '1M')
+
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/customize_airootfs.sh"]="0:0:755"
+)
